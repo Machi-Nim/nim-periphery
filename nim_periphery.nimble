c4008a7be72a60ace1545f223de222d9e6f2d167
ysaak-pudim <ysaaktech@gmail.com>
2026-06-30 19:43:40 -0300

    initial project structure: basic definitions and C libraries from `c_periphery`

---
+++ nim_periphery.nimble
@@ -0,0 +1,12 @@
+# Package
+
+version       = "0.1.0"
+author        = "ysaak-pudim"
+description   = "A Nim wrapper for c-periphery, but with great Nim features. Used as the machinim's codebase. On initial development stage yet."
+license       = "MIT"
+srcDir        = "src"
+
+# Dependencies
+
+requires "nim >= 2.2.8"
+requires "https://github.com/Machi-Nim/todo >= 0.1.1"
---
+++ src/nim_periphery/periphery_raw.nim
@@ -0,0 +1,32 @@
+import std/os
+
+import todo
+
+const c_periphery = "c_periphery"
+#proc `/`(a: string, b: string): string = a & "/" & b   # this functionality already exists
+
+# --- Compilation config ---
+# 1. Flag to include the headers path
+{.passC: "-I" & currentSourcePath().parentDir() / c_periphery.}
+
+# 2. Flag for c-periphery to use the modern hardware driver (cdev)
+{.passC: "-DPERIPHERY_GPIO_CDEV_SUPPORT=1".}
+
+# 3. Instruction to compile .c files with the project
+{.compile: c_periphery / "gpio_cdev_v1.c".}
+{.compile: c_periphery / "gpio_cdev_v2.c".}
+{.compile: c_periphery / "gpio_sysfs.c".}
+{.compile: c_periphery / "gpio.c".}
+{.compile: c_periphery / "i2c.c".}
+{.compile: c_periphery / "led.c".}
+{.compile: c_periphery / "mmio.c".}
+{.compile: c_periphery / "pwm.c".}
+{.compile: c_periphery / "serial.c".}
+{.compile: c_periphery / "spi.c".}
+{.compile: c_periphery / "version.c".}
+
+# --- Type definitions (C mapping) ---
+todo "TODO: Map C type definitions from c_periphery"
+
+# --- Functions (Explicit Bridge) ---
+todo "TODO: All the bridge logic for the C functions"
